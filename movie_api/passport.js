362e7a657b1954c8c70b1f835b1458d6fc231583
bmichu <57345730+bmichu@users.noreply.github.com>
2020-03-01 19:06:26 +0100

    Update passport.js

--- movie_api/passport.js
+++ movie_api/passport.js
@@ -28,7 +28,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
- secretOrKey: 'your_jwt_secret'
+ secretOrKey: 'michal@secret'
 }, (jwtPayload, callback) => {
  return Users.findById(jwtPayload._id)
  .then((user) => {
