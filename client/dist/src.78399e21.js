335b2988c3991269285a679075645b358fe2fa9b
bmichu <57345730+bmichu@users.noreply.github.com>
2020-06-07 20:51:15 +0200

    changes

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -34832,7 +34832,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51129" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53222" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
